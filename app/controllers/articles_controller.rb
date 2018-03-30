e0d2423647d48ed2fe44305b9e2480023c7bd2ad
Kartikey <sonikartikey005@gmail.com>
2018-03-30 11:01:55 +0530

    crud operation

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles =Article.all
+  end
+  def new 
+
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    if @article.save
+      flash[:notice] ="article is successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+ 
+  end
+
+  def show
+    @article =Article.find(params[:id])
+  end  
+  def edit
+    @article =Article.find(params[:id])
+  end
+  def update
+    @article =Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] ="article is successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+  def destroy
+    @article =Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "article was successfully deleted"
+    redirect_to articles_path
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
+end
